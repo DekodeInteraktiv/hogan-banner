24507a8441fe61b2b1c674313a571af6cd4d7b9d
pederan <peder1976@gmail.com>
2018-01-29 13:01:49 +0100

    One div that wraps both buttons and wrapped each button in a span

--- assets/template.php
+++ assets/template.php
@@ -15,13 +15,14 @@
  */
 
 declare( strict_types = 1 );
+
 namespace Dekode\Hogan;
 
 if ( ! defined( 'ABSPATH' ) || ! ( $this instanceof Banner ) ) {
 	return; // Exit if accessed directly.
 }
 ?>
-<?php if ( 'large' === $this->image_size && ! empty( $this->image_src ) ) : ?>
+	<?php if ( 'large' === $this->image_size && ! empty( $this->image_src ) ) : ?>
 	<div class="opacity-<?php echo esc_attr( $this->overlay_opacity ); ?>" style="background-image: url('<?php echo esc_url( $this->image_src ); ?>');">
 <?php endif; ?>
 
@@ -40,40 +41,53 @@ if ( ! defined( 'ABSPATH' ) || ! ( $this instanceof Banner ) ) {
 	<div class="column">
 		<?php
 		if ( ! empty( $this->tagline ) ) {
-			printf( '<div class="tagline">%s</div>',
+			printf(
+				'<div class="tagline">%s</div>',
 				esc_html( $this->tagline )
 			);
 		}
 
 		if ( ! empty( $this->heading ) ) {
-			hogan_component( 'heading', [
-				'title' => $this->heading,
-			] );
+			hogan_component(
+				'heading', [
+					'title' => $this->heading,
+				]
+			);
 		}
 
 		if ( ! empty( $this->content ) ) {
-			echo wp_kses( $this->content, [
-				'p'  => [],
-				'br' => [],
-			] );
+			echo wp_kses(
+				$this->content, [
+					'p'  => [],
+					'br' => [],
+				]
+			);
 		}
 
+		$call_to_action_buttons = [];
+
 		if ( ! empty( $this->call_to_action ) ) {
-			echo '<div>';
-			hogan_component( 'button', $this->call_to_action );
-			echo '</div>';
+			$call_to_action_buttons[] = $this->call_to_action;
 		}
 
 		if ( ! empty( $this->secondary_call_to_action ) ) {
+			$call_to_action_buttons[] = $this->secondary_call_to_action;
+		}
+
+		if ( ! empty( $call_to_action_buttons ) ) {
 			echo '<div>';
-			hogan_component( 'button', $this->secondary_call_to_action );
+			foreach ( $call_to_action_buttons as $button ) {
+				echo '<span>';
+				hogan_component( 'button', $button );
+				echo '</span>';
+			}
 			echo '</div>';
 		}
 
 		?>
 	</div>
 
-<?php
-if ( 'large' === $this->image_size && ! empty( $this->image_src ) ) {
-	echo '</div>';
-}
+	<?php
+	if ( 'large' === $this->image_size && ! empty( $this->image_src ) ) {
+		echo '</div>';
+	}
