3d89a872adbfcb174723f0fdf46a0a3280392f59
pederan <peder1976@gmail.com>
2018-06-26 13:46:27 +0200

    Fix issue#27 - Not render content div if no content

--- assets/template.php
+++ assets/template.php
@@ -28,63 +28,50 @@ if ( ! defined( 'ABSPATH' ) || ! ( $this instanceof Banner ) ) {
 	<div class="<?php echo esc_attr( hogan_classnames( 'hogan-banner-column hogan-banner-image', [ 'hogan-banner-dim-image' => $this->dim_image ] ) ); ?>">
 		<?php
 		if ( ! empty( $this->main_image ) ) {
-			echo wp_get_attachment_image(
-				$this->main_image['id'],
-				$this->main_image['size'],
-				$this->main_image['icon'],
-				$this->main_image['attr']
-			);
+			echo wp_get_attachment_image( $this->main_image['id'], $this->main_image['size'], $this->main_image['icon'], $this->main_image['attr'] );
 		}
 		?>
 	</div>
-	<div class="hogan-banner-column hogan-banner-content">
-		<div class="hogan-banner-content-inner">
-			<?php
-			if ( ! empty( $this->image ) ) {
-				echo wp_get_attachment_image(
-					$this->image['id'],
-					$this->image['size'],
-					$this->image['icon'],
-					$this->image['attr']
-				);
-			}
-
-			if ( ! empty( $this->tagline ) ) {
-				printf(
-					'<div class="hogan-tagline">%s</div>',
-					esc_html(
-						$this->tagline
-					)
-				);
-			}
-
-			if ( ! empty( $this->heading ) ) {
-				hogan_component(
-					'heading', [
-						'title' => $this->heading,
-					]
-				);
-			}
-
-			if ( ! empty( $this->content ) ) {
-				echo wp_kses(
-					$this->content, [
-						'p'  => [],
-						'br' => [],
-					]
-				);
-			}
-
-			if ( ! empty( $this->call_to_actions ) ) {
-				echo '<div class="hogan-banner-cta">';
-				foreach ( $this->call_to_actions as $button ) {
-					echo '<span>';
-					hogan_component( 'button', $button );
-					echo '</span>';
+	<?php if ( ! empty( $this->image ) || ! empty( $this->tagline ) || ! empty( $this->heading ) || ! empty( $this->content ) || ! empty( $this->call_to_actions ) ) : ?>
+		<div class="hogan-banner-column hogan-banner-content">
+			<div class="hogan-banner-content-inner">
+				<?php
+				if ( ! empty( $this->image ) ) {
+					echo wp_get_attachment_image( $this->image['id'], $this->image['size'], $this->image['icon'], $this->image['attr'] );
 				}
-				echo '</div>';
-			}
-			?>
+
+				if ( ! empty( $this->tagline ) ) {
+					printf( '<div class="hogan-tagline">%s</div>', esc_html( $this->tagline ) );
+				}
+
+				if ( ! empty( $this->heading ) ) {
+					hogan_component(
+						'heading', [
+							'title' => $this->heading,
+						]
+					);
+				}
+
+				if ( ! empty( $this->content ) ) {
+					echo wp_kses(
+						$this->content, [
+							'p'  => [],
+							'br' => [],
+						]
+					);
+				}
+
+				if ( ! empty( $this->call_to_actions ) ) {
+					echo '<div class="hogan-banner-cta">';
+					foreach ( $this->call_to_actions as $button ) {
+						echo '<span>';
+						hogan_component( 'button', $button );
+						echo '</span>';
+					}
+					echo '</div>';
+				}
+				?>
+			</div>
 		</div>
-	</div>
+	<?php endif; ?>
 </div>
