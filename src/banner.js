158460c23a071f9a4795fa799dd96aa52176994f
Peder Andreas Nielsen <peder1976@gmail.com>
2019-01-08 13:02:45 +0100

    Object fit fix - waiting for DOM loaded (#39)

--- src/banner.js
+++ src/banner.js
@@ -1,4 +1,14 @@
-const { objectFitImages } = window;
+function apply_object_fit_fix() {
+	if ('objectFit' in document.documentElement.style === false) {
+		const { objectFitImages } = window;
 
-const images = document.querySelectorAll( '.hogan-banner-image img' );
-objectFitImages( images );
+		const images = document.querySelectorAll( '.hogan-banner-image img' );
+		objectFitImages( images );
+	}
+}
+
+if (document.readyState === "loading") {
+	document.addEventListener("DOMContentLoaded", apply_object_fit_fix);
+} else {
+	apply_object_fit_fix();
+}
